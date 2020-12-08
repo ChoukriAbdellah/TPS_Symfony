465da3499cf11496e1405ffae47b7c90022ca384
choukri <abdellahchoukri2.0@gmail.com>
2020-12-09 00:43:14 +0100

    files for test removed

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -35,21 +35,5 @@ class ApiController extends AbstractController
         return $response;
     }
 
-    public function index(EntityManagerInterface $em, PaginatorInterface $paginator, Request $request): Response
-    {
-     
-        $dql   = "SELECT a.id, a.titre, a.imageName , a.description, a.dateMAJ from App:Article a order by a.dateCreation desc ";
-        $query = $em->createQuery($dql);
-    
-        $pagination = $paginator->paginate(
-            $query, /* query NOT result */
-            $request->query->getInt('page', 1), /*page number*/
-            3 /*limit per page*/
-        );
-     
-         // parameters to template
-         return $this->render('blog/index2.html.twig', [
-            'pagination' => $pagination,
-        ]);
-}
+
 }
